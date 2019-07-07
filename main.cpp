d1d0deeb8ea98e183cf42cba00a498f1be7a7402
Diego Nieto <diego.nieto.m@outlook.com>
2019-07-07 20:10:33 +0200

    Clamping

--- main.cpp
+++ main.cpp
@@ -11,24 +11,27 @@ static const char* pVS = "                                                    \n
                                                                               \n\
 layout (location = 0) in vec3 Position;                                       \n\
                                                                               \n\
-//uniform float glScale;                                                        \n\
-uniform mat4 gWorld;                                                        \n\
+//uniform float glScale;                                                      \n\
+uniform mat4 gWorld;                                                          \n\
+out vec4 Color;                                                               \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    //gl_Position = vec4(glScale * Position.x, Position.y, Position.z, 1.0);    \n\
-    gl_Position = gWorld * vec4(Position, 1.0);    \n\
+    //gl_Position = vec4(glScale * Position.x, Position.y, Position.z, 1.0);  \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                               \n\
+    Color = vec4(clamp(Position, 0.0, 1.0), 1.0);                             \n\
 }";
 
 // Fragment shared. This will modify the pixel colors
 static const char* pFS = "                                                    \n\
 #version 330                                                                  \n\
                                                                               \n\
+in vec4 Color;                                                                \n\
 out vec4 FragColor;                                                           \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    FragColor = vec4(0.0, 0.0, 1.0, 0.8);                                     \n\
+    FragColor = Color;                                                        \n\
 }";
 
 
